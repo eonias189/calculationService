99df8a41e924d0e6c192ef5be9dc5d6547093924
eonias189 <eonias189@yandex.ru>
2024-02-17 22:32:02 +0400

    navbar edited

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -16,16 +16,18 @@ interface NavbarProps {
 const Navbar: FC<NavbarProps> = ({ links }) => {
     const pathName = usePathname();
     return (
-        <nav className="flex flex-row">
+        <nav className="flex flex-row mx-auto mt-2 gap-2  w-4/5 rounded-lg">
             {links.map((link, key) => (
                 <Link
                     key={key}
                     href={link.href}
-                    className={`border-solid border-2 border-teal-600 rounded-xl mx-2 mt-2 px-1 ${cx({
-                        "bg-teal-600": pathName === link.href,
-                    })}`}
+                    className={`rounded-md hover:border-teal-500 p-1 relative overflow-hidden after:size-full after:absolute after:top-0 after:left-0 after:-translate-x-full after:z-10 after:bg-teal-500  hover:after:translate-x-0 after:transition-all after:duration-300 ${cx(
+                        {
+                            "bg-teal-500": pathName === link.href,
+                        }
+                    )}`}
                 >
-                    {link.name}
+                    <span className="relative z-20 -translate-x-1/2 -translate-y-1/2">{link.name}</span>
                 </Link>
             ))}
         </nav>
