91e9c365e77c40f2e2999f3d735e38f8742e68e0
eonias189 <eonias189@yandex.ru>
2024-02-17 19:40:42 +0400

    agents page

--- frontend/src/app/agents/page.tsx
+++ frontend/src/app/agents/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 import { fetchAgents } from "@/api";
+import Agent from "@/components/agent";
 import { contract } from "@/contract";
+import { useFetcher } from "@/hooks/useFetcher";
 import { FC, useState, useEffect } from "react";
 
 const Agents: FC = () => {
-    const [agents, setAgents] = useState<contract.AgentData[]>();
+    const agentsFetcher = useFetcher(fetchAgents);
     useEffect(() => {
-        fetchAgents().then(setAgents);
+        fetchAgents().then(agentsFetcher.update);
     }, []);
     return (
         <>
-            <p>agents</p>
-            {agents?.map((agent) => (
-                <p key={agent.id}>
-                    {agent.ping}, {agent.status.executingThreads}, {agent.status.maxThreads}
-                </p>
-            ))}
+            <h1 className="text-center font-normal text-2xl mt-4">Servers</h1>
+            <div className="flex flex-col gap-1 ml-2 items-center">
+                {agentsFetcher.data?.map((agent) => (
+                    <Agent key={agent.id} agent={agent} />
+                ))}
+            </div>
         </>
     );
 };
