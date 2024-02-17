69dbeea44200e512ee07e7ddcba66d2f16036a48
eonias189 <eonias189@yandex.ru>
2024-02-17 21:35:41 +0400

    timeouts page

--- frontend/src/app/timeouts/page.tsx
+++ frontend/src/app/timeouts/page.tsx
@@ -1,33 +1,66 @@
 "use client";
-import { fetchTimeouts, setTimeouts } from "@/api";
+import { fetchTimeouts, setTimeouts as sendTimeouts } from "@/api";
 import { contract } from "@/contract";
+import { useFetcher } from "@/hooks/useFetcher";
+import Input from "@/components/ui/input";
 import { FC, useEffect, useState } from "react";
+import Button from "@/components/ui/button";
+
+const entries = <T extends Object>(obj: T): [keyof T, T[keyof T]][] => {
+    const res: [keyof T, T[keyof T]][] = [];
+    for (let key in obj) {
+        res.push([key, obj[key]]);
+    }
+    return res;
+};
 
 const Timeouts: FC = () => {
-    const [timeouts, SetTimeouts] = useState<contract.Timeouts>();
+    const timeoutsFetcher = useFetcher(fetchTimeouts);
+    const [timeouts, setTimeouts] = useState(new contract.Timeouts().toObject());
     useEffect(() => {
-        fetchTimeouts().then(SetTimeouts);
+        timeoutsFetcher.update();
     }, []);
+    useEffect(() => {
+        setTimeouts({
+            ...timeouts,
+            add: timeoutsFetcher.data?.add ?? 0,
+            substract: timeoutsFetcher.data?.substract ?? 0,
+            multiply: timeoutsFetcher.data?.multiply ?? 0,
+            divide: timeoutsFetcher.data?.divide ?? 0,
+        });
+    }, [timeoutsFetcher.data]);
     return (
         <>
-            <p>timeouts</p>
-            <p>Add: {timeouts?.add}</p>
-            <p>Substract: {timeouts?.substract}</p>
-            <p>Multiply: {timeouts?.multiply}</p>
-            <p>Divide: {timeouts?.divide}</p>
-            <button
-                onClick={() => {
-                    const t = new contract.Timeouts();
-                    t.add = 4;
-                    t.substract = 3;
-                    t.multiply = 2;
-                    t.divide = 1;
-                    setTimeouts(t);
-                }}
-                className="bg-teal-500 rounded p-1"
-            >
-                button
-            </button>
+            <h1 className="text-center font-normal text-2xl mt-4">Timeouts</h1>
+            <form className="flex flex-col gap-1 ml-2">
+                {entries(timeouts).map(([key, value], num) => (
+                    <div key={num} className="flex flex-row items-center gap-2">
+                        <label>{key}</label>
+                        <Input
+                            type="range"
+                            max={100}
+                            value={value}
+                            onChange={(e) => {
+                                e.preventDefault();
+                                setTimeouts({
+                                    ...timeouts,
+                                    [key]: parseInt(e.currentTarget.value || "0"),
+                                });
+                            }}
+                        />
+                        <label>{value}</label>
+                    </div>
+                ))}
+                <Button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        sendTimeouts(contract.Timeouts.fromObject(timeouts));
+                    }}
+                    className="w-min text-nowrap"
+                >
+                    set timeouts
+                </Button>
+            </form>
         </>
     );
 };
